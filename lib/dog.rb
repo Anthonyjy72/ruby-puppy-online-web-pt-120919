f6b92c6ae946fecb27effcba1fb5b467abc4b69d
Anthony Ybarra <anthony_jy72@yahoo.com>
2020-02-02 18:17:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,72 @@
-class Dog
+class Shelter
+  attr_accessor :name, :city
+  @@all = []
 
-	@@all = []
-	attr_accessor :name
-
-	def initialize(name)
-		@name = name
-		@@all << self
-	end
-
-	def self.clear_all
-		@@all.clear
-
-	end
-
-	def self.all
-		@@all.each do |dog|
-			puts dog.name
-		end
-	end
+  def initialize (name, city)
+    @name = name
+    @city = city
+    @dogs = [] 
+    @@all << self 
+  end 
+  
+  def add_dog(dog)
+    dog.shelter = self
+    @dogs << dog 
+  end 
+  
+  def dogs 
+    @dogs 
+  end 
+  
+  def self.all 
+    @@all
+  end 
 end 
+
+class Dog
+  attr_accessor :name, :age, :breed, :shelter, :owner
+  @@all = []
+  
+  def initialize(name, age, breed)
+    @name = name
+    @age = age
+    @breed = breed 
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all
+  end 
+end 
+
+
+class Owner
+  attr_accessor :name, :age
+  attr_reader :dogs 
+  @@all = []
+  
+   def initialize (name, age)
+    @name = name
+    @age = age
+    @dogs = []
+    @shelters = []
+    @@all << self 
+  end 
+  
+  def add_dog(dog)
+    @dogs << dog 
+    dog.owner = self 
+  end 
+  
+  def shelters
+    self.dogs.each do |dog| 
+      @shelters << dog.shelter if dog.shelter
+    end 
+    @shelters
+  end
+  
+  def self.all 
+    @@all
+  end 
+end
+
